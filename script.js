97242f172db663e198cf91af7ae1d7719f482122
rubenjumbo06 <rapr46790@gmail.com>
2024-10-31 15:37:02 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Aquí puedes agregar cualquier funcionalidad interactiva que necesites
+document.querySelector('.map-btn').addEventListener('click', function() {
+    window.open('https://www.google.com/maps/search/?api=1&query=20840,Piura,Perú', '_blank');
+});
